397a71c15d59f98f8a2bf3f2c1a42cce3af18256
jyhyun1008 <jayhyun1008@gmail.com>
2025-12-30 20:21:20 +0900

    배포방식 추가(.gitignore 상태라 안보임)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  nitro: {
+    // 빌드 결과물이 저장될 폴더 경로를 지정합니다.
+    output: {
+      dir: 'featherVMC/.output', // 원하는 폴더명으로 변경 (예: 'dist', 'build/out' 등)
+      serverDir: 'featherVMC/.output/server',
+      publicDir: 'featherVMC/.output/public'
+    }
+  }
 })
