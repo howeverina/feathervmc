3aa5106f6bec1d966f7b933167afe12850f5a6aa
jyhyun1008 <jayhyun1008@gmail.com>
2025-12-31 07:28:57 +0900

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,9 +5,9 @@ export default defineNuxtConfig({
   nitro: {
     // 빌드 결과물이 저장될 폴더 경로를 지정합니다.
     output: {
-      dir: 'featherVMC/.output', // 원하는 폴더명으로 변경 (예: 'dist', 'build/out' 등)
-      serverDir: 'featherVMC/.output/server',
-      publicDir: 'featherVMC/.output/public'
+      dir: 'featherVMC/output', // 원하는 폴더명으로 변경 (예: 'dist', 'build/out' 등)
+      serverDir: 'featherVMC/output/server',
+      publicDir: 'featherVMC/output/public'
     }
   }
 })
